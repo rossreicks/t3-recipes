e95271a05237eb949abd8943a179c986d12f9668
Ross Reicks <rreicks13@gmail.com>
2024-01-14 17:57:53 -0600

    chore: switch to github actions

--- sst.config.ts
+++ sst.config.ts
@@ -1,3 +1,4 @@
+import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager';
 import { SSTConfig } from "sst";
 import { NextjsSite } from "sst/constructs";
 
@@ -10,7 +11,18 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site", {
+        customDomain: {
+            cdk: {
+                certificate: new Certificate(stack, "Certificate", {
+                    domainName: "*.rossreicks.com",
+                    validation: CertificateValidation.fromDns(),
+                }),
+            },
+            domainName: "t3-recipes.rossreicks.com",
+            isExternalDomain: true,
+        }
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
