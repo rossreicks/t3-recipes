dbd59b12f5eb03a152728274627d9620d0d897b1
Ross Reicks <rreicks13@gmail.com>
2024-01-14 20:03:54 -0600

    chore: try to map environment variables

--- sst.config.ts
+++ sst.config.ts
@@ -1,32 +1,38 @@
-import { Certificate, CertificateValidation } from 'aws-cdk-lib/aws-certificatemanager';
+import {
+	Certificate,
+	CertificateValidation,
+} from "aws-cdk-lib/aws-certificatemanager";
 import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+import { Config, NextjsSite } from "sst/constructs";
 
 export default {
-  config(_input) {
-    return {
-      name: "t3-recipes",
-      region: "us-east-1",
-    };
-  },
-  stacks(app) {
-    app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site", {
-        customDomain: {
-            cdk: {
-                certificate: new Certificate(stack, "Certificate", {
-                    domainName: "*.rossreicks.com",
-                    validation: CertificateValidation.fromDns(),
-                }),
-            },
-            domainName: "t3-recipes.rossreicks.com",
-            isExternalDomain: true,
-        }
-      });
+	config(_input) {
+		return {
+			name: "t3-recipes",
+			region: "us-east-1",
+		};
+	},
+	stacks(app) {
+		app.stack(function Site({ stack }) {
+			const site = new NextjsSite(stack, "site", {
+				customDomain: {
+					cdk: {
+						certificate: new Certificate(stack, "Certificate", {
+							domainName: "*.rossreicks.com",
+							validation: CertificateValidation.fromDns(),
+						}),
+					},
+					domainName: "t3-recipes.rossreicks.com",
+					isExternalDomain: true,
+				},
+				environment: {
+					DATABASE_URL: process.env.DATABASE_URL!,
+				},
+			});
 
-      stack.addOutputs({
-        SiteUrl: site.url,
-      });
-    });
-  },
+			stack.addOutputs({
+				SiteUrl: site.url,
+			});
+		});
+	},
 } satisfies SSTConfig;
